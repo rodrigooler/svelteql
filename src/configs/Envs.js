8beb34b8c2b9a96b52291241c2b0d89aab4d1950
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-07-27 03:53:56 -0300

    create project

---
+++ src/configs/Envs.js
@@ -0,0 +1,3 @@
+export default {
+  GRAPHQL_URI: "",
+};
---
+++ src/containers/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import ApolloClient from "apollo-boost";
+  import {
+    setClient,
+    getClient,
+    query as SveltQuery,
+    mutate as SveltMutate
+  } from "svelte-apollo";
+
+  import { GRAPHQL_URI } from "../configs/Envs";
+
+  setClient(
+    new ApolloClient({
+      uri: GRAPHQL_URI
+    })
+  );
+
+  export const client = getClient();
+  export const query = query => SveltQuery(client, { query });
+  export const mutate = (mutate, variables) =>
+	SveltMutate(client, { mutate, variables });
+	
+	
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
